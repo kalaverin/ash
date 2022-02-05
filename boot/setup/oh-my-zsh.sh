7e4a55e12899408ac245a5d0829c9381aef8316f
Aleksey Ermolin <a_ermolin@wargaming.net>
2022-02-06 01:41:24 +0300

    develop: debug

--- boot/setup/oh-my-zsh.sh
+++ boot/setup/oh-my-zsh.sh
@@ -130,7 +130,7 @@ else
                         continue
 
                     else
-                        $(GIT_TERMINAL_PROMPT=0 $git --git-dir="$dst/.git" --work-tree="$dst/" pull origin "$branch")
+                        $commands[zsh] -c "GIT_TERMINAL_PROMPT=0 $git --git-dir='$dst/.git' --work-tree='$dst/'' pull origin '$branch'"
                         [ -x "`which git-restore-mtime`" ] && \
                             git-restore-mtime --skip-missing --work-tree "$dst/" --git-dir "$dst/.git/"
                     fi
